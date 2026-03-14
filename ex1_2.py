883069a7763d3ee5df3af6ad72b4caa6eec1713a
Nguyen Dung <ndung030507@gmail.com>
2026-03-14 23:12:21 +0700

    ex1.2

---
+++ ex1_2.py
@@ -0,0 +1,20 @@
+# Exercise 1 - Think Python Chapter 1
+
+# Cau 1: 42 phut 42 giay bang bao nhieu giay
+
+minutes = 42
+seconds = 42
+total_seconds = minutes * 60 + seconds
+print("Cau 1:", total_seconds, "giay")
+
+# Cau 2: 10 km bang bao nhieu mile
+
+km = 10
+mile = km / 1.61
+print("Cau 2:", mile, "mile")
+
+# Cau 3: Toc do trung binh
+
+time_hours = total_seconds / 3600
+speed = mile / time_hours
+print("Cau 3:", speed, "mile/giờ")
